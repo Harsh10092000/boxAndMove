5d64873d0941b5603ff8b4eae9be9000947fced9
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-11-20 16:31:11 +0530

    commit

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -112,27 +112,144 @@
 
 
 
-import { unstable_cache } from 'next/cache'
-import Link from 'next/link'
+// import { unstable_cache } from 'next/cache'
+// import Link from 'next/link'
  
-const getPosts = unstable_cache(
-  async () => {
-    let data = await fetch('https://api.vercel.app/blog')
-    let posts = await data.json()
-    return posts;
-  },
-  ['posts'],
-  { revalidate: 3600, tags: ['posts'] }
-)
+// const getPosts = unstable_cache(
+//   async () => {
+//     let data = await fetch('https://api.vercel.app/blog')
+//     let posts = await data.json()
+//     return posts;
+//   },
+//   ['posts'],
+//   { revalidate: 3600, tags: ['posts'] }
+// )
  
-export default async function Page() {
-  const allPosts = await getPosts()
- console.log("allPosts : " , allPosts);
+// export default async function Page() {
+//   const allPosts = await getPosts()
+//  console.log("allPosts : " , allPosts);
+//   return (
+//     <ul>
+//       {allPosts.map((post) => (
+//         <li key={post.id}><Link href={`/test/${post.title}`}>{post.title}</Link> </li>
+//       ))}
+//     </ul>
+//   )
+// }
+
+
+
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import React from "react";
+import pool from "../libs/mysql";
+import Link from "next/link";
+
+
+const getData = async (currentPage) => {
+  try {
+    const db = await pool;
+    const query =
+      "SELECT * from experts LEFT JOIN expert_subjects ON experts.SubjectID = expert_subjects.sid WHERE experts.id > ? limit 12";
+    const q = "SELECT COUNT(*) as total from experts";
+
+    const [rows] = await db.query(query, (currentPage - 1) * 12);
+    const [total] = await db.query(q);
+    return { data: rows, total: total };
+  } catch (error) {
+    return error;
+  }
+};
+
+const Page = async ({ searchParams }) => {
+  const currentPage = searchParams["page"] || 1;
+  const res = await getData(currentPage);
+  // console.log(res);
+  const recordsPerPage = 12;
+  const nPages = Math.ceil(res.total[0].total / recordsPerPage);
+
   return (
-    <ul>
-      {allPosts.map((post) => (
-        <li key={post.id}><Link href={`/test/${post.title}`}>{post.title}</Link> </li>
-      ))}
-    </ul>
-  )
-}
+    <div>
+      <Header />
+      <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] pb-20">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+          <div className="mt-5 max-w-2xl text-center mx-auto">
+            <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+              Hire Our
+              <span className="bg-clip-text bg-gradient-to-tl from-blue-500 to-violet-500 text-transparent">
+                &nbsp;Experts
+              </span>
+            </h1>
+          </div>
+
+          <div></div>
+
+          <div className="mt-5 max-w-3xl text-center mx-auto">
+            <p className="text-lg text-gray-500 dark:text-gray-400">
+              This is the expert section of examhelp , if you want any help do
+              contact us here and hire any freelance experts available widely.
+            </p>
+          </div>
+        </div>
+      </div>
+      <section className="max-w-[1350px] mx-auto px-4 sm:px-6 lg:px-4 py-12 ">
+        <div className="text-center pb-12">
+          <h2 className="text-xl font-bold text-indigo-500">
+            Having Highest Rating
+          </h2>
+          <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900 ">
+            Check our awesome team members
+          </h1>
+        </div>
+        <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-10">
+          {/* <Card /> */}
+          {res.data.map((item, index) => (
+            <div class="max-w-3xl w-full mx-auto z-10" key={index}>
+              <div class="bg-white border border-white shadow-lg  rounded-3xl p-4 m-4">
+                <div class="flex-none sm:flex gap-3">
+                  
+                  <div class="flex flex-col">
+                   
+                          <div class="w-full flex-none text-lg text-gray-800 font-bold leading-none">
+                            {item.Name}
+                          </div>
+                          <div class="flex-auto text-gray-500 my-1">
+                            <span class="mr-3 capitalize">{item.Title}</span>
+                         
+                    </div>
+                   
+                    <div class="flex pt-2  text-sm text-gray-500">
+                      <Link
+                        href={`/experts/${
+                          item.Name.toLowerCase().replaceAll(" ", "-") +
+                          "-" +
+                          item.id
+                        }`}
+                        className="shadow shadow-blue-600 text-blue-600 py-1 px-2 rounded hover:bg-blue-600 hover:text-white transition-all duration-300 ease-in-out"
+                      >
+                        Hire Expert
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <div
+        className="flex justify-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center
+                     text-gray-900 uppercase align-middle transition-all rounded-full select-none
+                      disabled:pointer-events-none 
+                      disabled:opacity-50 disabled:shadow-none"
+      >
+       
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
